b5b4630af933a965f73be8233f717f9db0d34498
Miguel Angel Solano <m.a.solano93@gmail.com>
2018-10-13 17:13:45 -0500

    Resolves complete initial definiton of Radon in Typescript

--- types/siloNode.d.ts
+++ types/siloNode.d.ts
@@ -16,5 +16,34 @@ export interface SiloNode {
 
     subscribers: Array<any>;
 
-    
+    constructor(
+    name : string, 
+    value: any, 
+    parent: string | null, 
+    modifiers: object, 
+    type: string, 
+    devTool: any | null) : void; 
+
+    pushToSubscribers(renderFunction: Function) : void;
+
+    removeFromSubscribersAtIndex(index: number) : void;
+
+    issueID() : void;
+
+    notifySubscribers(): void;
+
+    runModifiers(): void;
+
+  deconstructObjectIntoSiloNodes(objName: string, objectToDeconstruct: any, parent: string | null, runLinkedMods: boolean) : object;
+
+  linkModifiers(nodeName: string, stateModifiers: any): void;
+
+  reconstruct(siloNodeName: any, currSiloNode: any) : any;
+
+  reconstructArray(siloNodeName: any, currSiloNode: any) : any;
+
+  reconstructObject(siloNodeName: any, currSiloNode: any) : any;
+
+  getState() : any;
+
 }
